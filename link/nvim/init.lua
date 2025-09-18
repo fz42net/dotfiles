73fa9c1a349f31d97e86948013f403ec62172cee
Phil Thomas <phil@fz42.net>
2025-09-18 06:50:17 -0400

    Adding lazy

--- link/nvim/init.lua
+++ link/nvim/init.lua
@@ -10,8 +10,4 @@ vim.o.updatetime = P.is_work() and 750 or 300
 pcall(require, "private")
 
 -- Plugin manager (lazy.nvim assumed)
-require("lazy").setup(require("plugins"), {
-  lockfile = P.is_work()
-    and (vim.fn.stdpath("state").."/lazy-lock.work.json")
-     or (vim.fn.stdpath("state").."/lazy-lock.personal.json")
-})
+require("lazy")
